867b84ae8133d71e3cb07c525ffeb881a57d3909
Protazer <florian.huberdeau@laposte.net>
2025-05-17 16:48:06 +0200

    test(login): Add login tests

--- front/src/app/features/auth/components/login/login.component.spec.ts
+++ front/src/app/features/auth/components/login/login.component.spec.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -11,15 +11,23 @@ import { expect } from '@jest/globals';
 import { SessionService } from 'src/app/services/session.service';
 
 import { LoginComponent } from './login.component';
+import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
+import {of, throwError} from "rxjs";
+
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let authService: AuthService;
+  let sessionService: SessionService;
+  let router: Router;
+  let formBuilder: FormBuilder;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
-      providers: [SessionService],
+      providers: [SessionService, AuthService],
       imports: [
         RouterTestingModule,
         BrowserAnimationsModule,
@@ -33,10 +41,61 @@ describe('LoginComponent', () => {
       .compileComponents();
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    authService = TestBed.inject(AuthService);
+    router = TestBed.inject(Router);
+    sessionService = TestBed.inject(SessionService);
+    formBuilder = TestBed.inject(FormBuilder);
+    component.form = formBuilder.group(({email: [
+        '',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.min(3)
+        ]
+      ]}))
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should submit the form with no error', () => {
+    fixture.ngZone?.run(() => {
+      const navigateSpy = jest.spyOn(router, 'navigate');
+      const authSpy = jest.spyOn(authService, 'login').mockReturnValue(of({token: 'jwt',
+        type: 'session',
+        id: 1,
+        username: 'user@user.com',
+        firstName: 'User',
+        lastName: 'User',
+        admin: false,}));
+      const sessionServiceSpy = jest.spyOn(sessionService, 'logIn');
+      component.form.setValue({email: 'yoga@studio.com', password: 'test!1234'});
+      component.submit();
+      expect(authSpy).toHaveBeenCalledWith({email: 'yoga@studio.com', password: 'test!1234'});
+      expect(sessionServiceSpy).toHaveBeenCalled();
+      expect(sessionService.sessionInformation).toStrictEqual({
+        token: 'jwt',
+        type: 'session',
+        id: 1,
+        username: 'user@user.com',
+        firstName: 'User',
+        lastName: 'User',
+        admin: false,
+      })
+      expect(navigateSpy).toHaveBeenCalledWith(['/sessions']);
+      expect(component.onError).toBeFalsy();
+    })
+  })
+  it('should submit the form with error', () => {
+    const authSpy = jest.spyOn(authService, 'login').mockReturnValueOnce(throwError(() => {}));
+    component.submit();
+    expect(component.onError).toBeTruthy();
+  })
 });
