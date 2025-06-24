7244013dba7a780528f0891c184dfb62ee63e047
florian <florian.huberdeau@laposte.net>
2025-06-24 23:51:54 +0200

    test(e2e): Add login e2e tests

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -1,7 +1,12 @@
-describe('Login spec', () => {
-  it('Login successfull', () => {
-    cy.visit('/login')
+/// <reference types="Cypress"/>
 
+describe('Login spec', () => {
+
+  beforeEach(() => {
+    cy.visit('/login');
+  })
+
+  it('Login successfull', () => {
     cy.intercept('POST', '/api/auth/login', {
       body: {
         id: 1,
@@ -21,7 +26,36 @@ describe('Login spec', () => {
 
     cy.get('input[formControlName=email]').type("yoga@studio.com")
     cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
-
     cy.url().should('include', '/sessions')
+  })
+  it("should return error if one field is invalid", () => {
+    cy.get('input[formControlName=email]').type("yoga@studio.com")
+    cy.get('input[formControlName=password]').type(`invalid {enter}{enter}`)
+    cy.get('.error').should('be.visible');
+  })
+
+  it("should logout user after login", () => {
+    cy.intercept('POST', '/api/auth/login', {
+      body: {
+        id: 1,
+        username: 'userName',
+        firstName: 'firstName',
+        lastName: 'lastName',
+        admin: true
+      },
+    })
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/session',
+      },
+      []).as('session')
+
+    cy.get('input[formControlName=email]').type("yoga@studio.com")
+    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+    cy.url().should('include', '/sessions');
+    cy.get('.link').contains('Logout').click();
+    cy.url().should('include', '/');
   })
 });
